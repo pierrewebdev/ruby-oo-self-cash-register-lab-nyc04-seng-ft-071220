0747e743af3135097e2d4e2067cea0798bb5ea14
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 21:33:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,58 +1,34 @@
 require "pry"
 class CashRegister
   
+  attr_accessor :discount, :items, :total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_item_price = nil
+  end
+  
+  def add_item(title, price ,quantity = 1)
+    self.total += price * quantity
+    quantity.times {self.items.push(title)}
+    last_item_price = price * quantity
+  end
+  
+  def apply_discount
+    if self.discount != 0
+      self.total -=  (self.total * (self.discount/100.0))
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-#   attr_accessor :discount, :items, :total
-  
-#   def initialize(discount = 0)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-  
-#   def add_item(title, price ,quantity = 1)
-#     self.total += (price * quantity)
-#     quantity.times do
-#       @items.push(title)
-#     end
-#   end
-  
-#   def apply_discount
-#     if @discount != 0
-#       @total -=  (@total * (@discount/100.0))
-#       #binding.pry
-#       "After the discount, the total comes to $#{@total.to_i}."
-#     else
-#       "There is no discount to apply."
-#     end
-#   end
-  
-  
-#   def void_last_transaction
-#     #binding.pry
-    
-#     end
-#     #binding.pry
-# end
+  def void_last_transaction
+    self.total -= self.last_item_price
+  end
 
 end #for whole class
 
