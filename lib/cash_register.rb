7c0ef9b62850dcbbd04133e0e6f95dd152bef712
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-04 21:33:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require "pry"
 class CashRegister
   
-  attr_accessor :discount, :items, :total, :las
+  attr_accessor :discount, :items, :total, :last_item_price
   
   def initialize(discount = 0)
     @total = 0
